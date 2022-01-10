c53d38cd8318c68ee340cc185410dd4d801ae77b
nancyaliyan <50418951+nancyaliyan@users.noreply.github.com>
2022-01-10 15:17:01 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,28 @@
+#assignment1
+#question1
+numb1=int(input("enter First number: "))
+numb2=int(input("enter second number: "))
+numb3=int(input("enter third number: "))
+avg=(numb1+numb2+numb3)/3
+print(avg)
+#question2
+
+
+
+
+
+#question3
+
+
+
+
+
+#question4
+
+
+
+
+
+
+
+#question5
