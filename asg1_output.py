fc03a90c14d6a2dd386d3dae96bdc3c7c9436281
nancyaliyan <50418951+nancyaliyan@users.noreply.github.com>
2022-01-10 15:17:54 +0530

    Add files via upload

---
+++ asg1_output.py
@@ -0,0 +1,28 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=10
+>>> b=20
+>>> a+b
+30
+>>> numb1=int(input("enter First number: "))
+enter First number: 23
+>>> numb2=int(input("enter second number: "))
+enter second number: 34
+>>> numb3=int(input("enter third number: "))
+enter third number: 45
+>>> avg=(numb1+numb2+numb3)/3
+>>> print("avg is:",avg)
+avg is: 34.0
+>>> 
+======================== RESTART: C:\Users\Asus\Desktop\q1.py =======================
+enter First number: 23
+enter second number: 34
+enter third number: 45
+avg is: 34.0
+>>> 
+======================== RESTART: C:\Users\Asus\Desktop\q1.py =======================
+enter First number: 34
+enter second number: 45
+enter third number: 56
+45.0
+>>> 
